5142bae1b3c03d05ddd0502351ba4302b67db776
gb005 <gehendrabhattarai@gmail.com>
2017-05-03 14:00:32 +0000

    using functions

--- gff2featureGC-01.py
+++ gff2featureGC-01.py
@@ -1,140 +1,181 @@
 
-# coding: utf-8
+#! /home/gb005/anaconda3/bin/python3.6
+# this program calculates the nucleotide composition for the features in a genome
 
-# In[444]:
+# load the system module
+import sys
+import collections
 
-# declare the file names
-gff_file = 'watermelon.gff'
-fsa_file = 'watermelon.fsa'
+andy = 42
+#print("Main-1:", andy)
 
-# open the files for reading
+# a function to clean up a DNA sequence
+def clean_seq(input_seq):
+    clean = input_seq.upper()
+    clean = clean.replace('N', '')
+    return clean
+
+
+def nuc_freq(sequence, base1, base2,sig_digs=2):
+    # Calculate the length of the sequence
+    length = len(sequence)
+    
+    # genome covered with each feature class
+    genome_cover=(len(sequence)/len(genome))*100
+    
+    # count the number of this nucleotide
+    base_1_count = sequence.count(base1)
+    
+    base_2_count = sequence.count(base2)
+
+    # Calculate the GC content
+    gc_content = ((base_1_count+base_2_count)/length)*100
+    # print(gc_content)
+    #feq_of_base = count_of_base/length
+    
+    # return the frequency and the length
+    return (length, genome_cover, round(gc_content, sig_digs))
+
+# function to get reverse complement sequences
+def reverse(dna_seq):
+    complement0 = (dna_seq.replace("A", "t"))
+    complement1 = (complement0.replace("C", "g"))
+    complement2 = (complement1.replace("T", "a"))
+    complement3 = (complement2.replace("G", "c"))
+    return (complement3.upper())
+
+usage = sys.argv[0] + ": watermelon.fsa watermelon.gff"
+
+if len(sys.argv) < 3:
+    print(usage)
+    sys.exit
+
+# read above arguments
+gff = sys.argv[1]
+fsa = sys.argv[2]
+#print(genome)
+
+gff_file = "watermelon.gff"
+fsa_file = "watermelon.fsa"
+
+#open the files
 gff_in = open(gff_file, 'r')
 fsa_in = open(fsa_file, 'r')
 
+# Creating dictionary
+# key = feature type, value = concatenation of all sequences of that type-not useful for anything other than calculating AT/GC content
 
-# declare variable that will hold the genome sequence
+feature_sequences={}
+
+#key exon, value sequence
+exon_sequences={}
+
+# key is gene and value is concatenation of all exon sequences in a gene
+gene_sequences={}
+
+#declare a variable
 genome = ''
 
-# initialize a line counter
 line_number = 0
 
-# read in the genome file
 for line in fsa_in:
     # print(str(line_number) + ": " + line)
-
-    # remove newline's - could also use strip
+    
     line = line.rstrip('\n')
-
+    #print(str(line_number) + ": " + line)
     if line_number > 0:
-        genome = genome + line
+        genome+=line
+        
+    line_number+=1
+    
+#check if we get genome correctly
+# print(len(genome))
 
-    # increment line_number
-    line_number += 1
+#close the fsa file
+fsa_in.close()
 
-# did we get the genome correctly?
-genome_length = len(genome)
-#print(genome_length)
-
-# close the genome file
-#fsa_in.close()
-
-cds     = ''
-trna    = ''
-rrna    = ''
-intron  = ''
-misc    = ''
+cds = ''
+trna = ''
+rrna = ''
+intron = ''
+misc = ''
 repeats = ''
 
-# read in the GFF file
 for line in gff_in:
-
-    # remove newline's - could also use strip
     line = line.rstrip('\n')
-
-    types = line.split('; type ')
+    types = line.split('type')
     other_type = types[len(types)-1]
-    # print(other_type)
+    #print(line)
+    #print(types)
+    #print(other_type)
     
     fields = line.split('\t')
-    type  = fields[2]
+    #print(fields)
+    type = fields[2]
+    #print(type)
+    strand = fields[6]
+    #print(strand)
+    
     start = int(fields[3])
-    end   = int(fields[4])
+    #print(start)
     
-    # print(type, "\t", start, "\t", end)
+    end = int(fields[4])
+    #print(end)
     
-    # extract this feature from the genome
+    # print(type, "\t",start, "\t",end)
+    
+    
+    # now extract features from the genome
     fragment = genome[start-1:end]
-
+    #print(fragment)
+    fragment = clean_seq(fragment)
+    #print(fragment)
+    
+    if type in feature_sequences:
+        feature_sequences[type] += fragment
+    else:
+        feature_sequences[type]=fragment
+        
     if type == 'CDS':
-        cds += fragment
+        gene_feature = fields[8]
+        gene1 = gene_feature.split(':')
+        gene = gene1[0]
+        gene_seq = genome[start-1:end]
+        #print(gene)
+        #print(gene_seq)
+        
+        
+        if strand == '-':
+            complement_sequence = reverse(gene_seq)
+            exon_sequences[gene] = complement_sequence
+        else:
+            exon_sequences[gene] = gene_seq
+    # print(exon_sequences)
+    # print(gene)
     
-    if type == 'intron':
-        intron += fragment
+#close tthe file
+gff_in.close()
 
-    if type == 'misc_feature':
-        misc += fragment
-        
-    if type == 'rRNA':
-        rrna += fragment  
-        
-    if type == 'repeat_region':
-        repeats += fragment
+# now need to order the exon sequences
+ordered_exons_sequences = collections.OrderedDict(sorted(exon_sequences.items()))
+
+#for exon, sequence in ordered_exons_sequences.items():
+    #print(">", exon, "\n", sequence)
+    # this prints the exons along with the sequences in fasta fmt
     
-    if type == 'tRNA':
-        trna += fragment
-        
-# print the output
-# print(cds.count('G'))
-# print(cds.count('C'))
+for feature, sequence in feature_sequences.items():
+    (feature_length, cover, feature_comp) = nuc_freq(sequence, base1='C',base2='G',sig_digs=2)
+    
+    print(feature, "\t" , str(len(sequence)), str(round(cover, 1)) + "%", "\t",str(feature_comp)+ "%")
+    
+    #print(feature, "\t", str(round(cover, 1)) +"%") # check
+    #feature_length = nuc_freq(sequence, base1, base2, sigs_digs=2) #check
+    #print(feature_length) #check
+    #print(feature + "          \t" + str(len(sequence))) #check
+   
 
-length_cds = len(cds)
-# print(length_cds)
-length_intron = len(intron)
-#print(length_intron)
-length_misc = len(misc)
-#print(length_misc)
-length_rRNA = len(rrna)
-#print(length_rRNA)
-length_repeats = len(repeats)
-#print(length_repeats)
-length_trna = len(trna)
-#print(length_trna)
 
-#percentage of genome covered by exon
-percent_exon = (length_cds/genome_length)*100
-# print('(',round(percent_exon,2),"%",')')
-GC_cds = (((cds.count('G') + cds.count('C'))/length_cds)*100)
-print("exon             ", length_cds ,"  ", '(',round(percent_exon,2),"%",')', "  ", round(GC_cds,2))
+# In[ ]:
 
-# percentage of genome covered by intron
-percent_intron = (length_intron/genome_length)*100
-GC_intron = (((intron.count('G') + intron.count('C'))/length_intron)*100)
-print("intron           ", length_intron ,"  ",'(',round(percent_intron,2),"%",')', "  ", round(GC_intron,2))
 
-# percentage of genome covered by misc_feature
-percent_misc = (length_misc/genome_length)*100
-GC_misc = (((misc.count('G') + misc.count('C'))/length_misc)*100)
-print("misc_feature     ", length_misc , "  ",'(',round(percent_misc,2),"%",')', "   ",round(GC_misc,2))
 
-# percentage of genome covered by rrna
-percent_rrna = (length_rRNA/genome_length)*100
-GC_rrna = (((rrna.count('G') + rrna.count('C'))/length_rRNA)*100)
-print("rRNA             ", length_rRNA , "  ",'(',round(percent_rrna,2),"%",')', "   ", round(GC_rrna,2))
-
-# percentage of genome covered by repat_region
-percent_repeats = (length_repeats/genome_length)*100 
-GC_repeats = (((repeats.count('G') + repeats.count('C'))/length_repeats)*100)
-print("repeat_region    ", length_repeats , "  ",'(',round(percent_repeats,2),"%",')', "  ", round(GC_repeats,2))
-
-# percentage of genome covered by repat_region
-percent_trna = (length_trna/genome_length)*100 
-GC_trna = (((trna.count('G') + trna.count('C'))/length_trna)*100)
-print("tRNA             ", length_trna , "  ",'(',round(percent_trna,2),"%",')', "   ", round(GC_trna,2))
-
-# output after running the codes are shown below:
-# exon              32442    ( 8.55 % )    43.12
-# intron            32476    ( 8.56 % )    51.37
-# misc_feature      24282    ( 6.4 % )     37.39
-# rRNA              5148    ( 1.36 % )     52.45
-# repeat_region     14572    ( 3.84 % )    45.66
-# tRNA              1358    ( 0.36 % )     50.22
